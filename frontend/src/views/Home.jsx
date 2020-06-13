e4500f6b4312a2e5c14f5137951a2c576a5269d8
Mugdha001 <mugdha.wadikar@sjsu.edu>
2020-06-13 11:51:04 -0700

    Edited some text on Home.jsx

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -27,13 +27,13 @@ import {
   CardFooter,
   CardTitle,
   Row,
-  Col
+  Col,
 } from "reactstrap";
 // core components
 import {
   dashboard24HoursPerformanceChart,
   dashboardEmailStatisticsChart,
-  dashboardNASDAQChart
+  dashboardNASDAQChart,
 } from "variables/charts.jsx";
 
 class Home extends React.Component {
@@ -41,51 +41,32 @@ class Home extends React.Component {
     return (
       <>
         <div className="content">
-          
           <Row>
             <Col md="12">
               <Card>
                 <CardHeader>
                   <CardTitle tag="h5">About Us</CardTitle>
-                  
                 </CardHeader>
                 <CardBody>
-                  <p>Sewa International is a Hindu faith-based, humanitarian, nonprofit service organization registered under Internal Revenue Code 501 (c) (3). Founded in 2003, Sewa International is part of a larger movement that started in India in 1989 and is active in twenty countries. Sewa serves humanity irrespective of race, color, religion, gender or nationality.
-We specialize in disaster relief and rehabilitation. Our development programs focus on family services; child, tribal and refugee welfare; women empowerment; health; and education. In addition to our work in the US, we have undertaken development projects in Colombia, Guyana, India, Kenya, Pakistan and Sri Lanka.
-
-</p>
+                  <p>What this application is about</p>
                 </CardBody>
-                
               </Card>
             </Col>
           </Row>
           <Row>
-            
+            <Col md="12"></Col>
+          </Row>
+          <Row>
             <Col md="12">
-              <Card className="card-chart">
-                <CardHeader>
-                  <CardTitle tag="h5">Our Website</CardTitle>
-                </CardHeader>
-                <CardBody>
-                <a href="https://sewausa.org/">
-  <img src={require('./sewa.PNG')} />
-</a>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-          <Row>
-          <Col md="12">
               <Card>
                 <CardHeader>
                   <CardTitle tag="h5">Contact Us</CardTitle>
                 </CardHeader>
                 <CardBody>
-                Address : Sewa International, P.O. Box 820867, Houston, Texas 
-                <br></br>
-                Contact Information : 77282-0867
+                  Address
+                  <br></br>
+                  Contact Information : 77282-0867
                 </CardBody>
-               
               </Card>
             </Col>
           </Row>
